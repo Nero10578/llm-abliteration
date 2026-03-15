b036f5cb8070b3cea4f181d190b5c86f0213b88f
Nero10578 <owenarliawan@gmail.com>
2026-03-14 19:03:19 -0700

    fp32

--- measure.py
+++ measure.py
@@ -343,7 +343,7 @@ if __name__ == "__main__":
     elif qbit == "8bit":
         quant_config = BitsAndBytesConfig(
             load_in_8bit=True,
-#            llm_int8_enable_fp32_cpu_offload=True,
+            llm_int8_enable_fp32_cpu_offload=True,
 #            llm_int8_has_fp16_weight=True,
         )    
 
